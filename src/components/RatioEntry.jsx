9a4efdce231d7c01d6489b6e972e2afe82203fd8
do-haru <doharu05@naver.com>
2025-11-22 22:30:24 +0900

    feat: RatioEntry 컴포넌트에 요소 추가 및 스타일 적용

--- src/components/RatioEntry.jsx
+++ src/components/RatioEntry.jsx
@@ -2,8 +2,12 @@ import "./RatioEntry.css";
 
 const RatioEntry = () => {
   return (
-    <div>
-      <p>item</p>
+    <div className="RatioEntry">
+      <input type="text" placeholder="재료" />
+      <p> : </p>
+      <input type="number" placeholder="값1" />
+      <p> : </p>
+      <input type="number" placeholder="값2" />
     </div>
   );
 };
