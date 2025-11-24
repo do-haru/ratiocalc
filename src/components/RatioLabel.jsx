3b1812a42921d7d6ff271f3c9de618be4421155b
do-haru <doharu05@naver.com>
2025-11-25 00:22:12 +0900

    feat: RatioLabel 컴포넌트에 요소 추가 및 스타일 적용

--- src/components/RatioLabel.jsx
+++ src/components/RatioLabel.jsx
@@ -1,7 +1,18 @@
 import "./RatioLabel.css";
 
 const RatioLabel = () => {
-  return <div>RatioLabel</div>;
+  return (
+    <div className="RatioLabel">
+      <div className="Label1">
+        <p>재료</p>
+      </div>
+      <div className="Label2">
+        <p>값1</p>
+        <p>:</p>
+        <p>값2</p>
+      </div>
+    </div>
+  );
 };
 
 export default RatioLabel;
